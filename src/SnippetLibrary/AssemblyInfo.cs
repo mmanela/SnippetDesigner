6bd53d9e5384d045fcd42136d2ce37e77b62000f
Martin Fetzel <martin.fetzel@get-the-solution.net>
2022-02-06 20:39:16 +0100

    csproj to new sdk style

--- src/SnippetLibrary/AssemblyInfo.cs
+++ src/SnippetLibrary/AssemblyInfo.cs
@@ -1,33 +1,6 @@
-// Copyright (C) Microsoft Corporation. All rights reserved.
+﻿// Copyright (C) Microsoft Corporation. All rights reserved.
 
 using System.Reflection;
-
-//
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//
-[assembly: AssemblyTitle("SnippetLibrary")]
-[assembly: AssemblyDescription("Snippet Library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Microsoft Corporation")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]		
-
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("1.3.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
