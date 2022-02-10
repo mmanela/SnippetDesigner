63de2181ed55dedb1ef3f6d1d1928b673b36d69c
Martin Fetzel <martin.fetzel@get-the-solution.net>
2022-02-10 21:22:53 +0100

    upgrade to vs17 and .net 4.8

--- src/SnippetDesigner/Properties/AssemblyInfo.cs
+++ src/SnippetDesigner/Properties/AssemblyInfo.cs
@@ -1,26 +1,3 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Snippet Designer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Snippet Designer")]
-[assembly: AssemblyCopyright("Microsoft Corporation")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]   
 [assembly: ComVisible(false)]
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("1.8.*")]
